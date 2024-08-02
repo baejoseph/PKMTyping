acc794cb852fa3dbcb8a381ac17db37894365ce1
baejoseph <baejoseph@gmail.com>
2024-08-02 15:44:33 +0100

    score icon scale

--- sprites.py
+++ sprites.py
@@ -2,10 +2,10 @@ import pygame
 from utils import resource_path
 
 class Sprites:
-    pkbimg = pygame.image.load(resource_path("assets/items/poke-ball.png"))
-    scoreimg = pygame.image.load(resource_path("assets/items/sapphire.png"))
-    comboimg = pygame.image.load(resource_path("assets/items/shiny-stone.png"))
-    mistakeimg = pygame.image.load(resource_path("assets/items/red-card.png"))
+    pkbimg = pygame.transform.scale(pygame.image.load(resource_path("assets/items/poke-ball.png")), (30, 30))
+    scoreimg = pygame.transform.scale(pygame.image.load(resource_path("assets/items/sapphire.png")), (25, 25))
+    comboimg = pygame.transform.scale(pygame.image.load(resource_path("assets/items/shiny-stone.png")), (30, 30))
+    mistakeimg = pygame.transform.scale(pygame.image.load(resource_path("assets/items/red-card.png")), (30, 30))
     masterball = pygame.transform.scale(pygame.image.load(resource_path("assets/items/gen5/master-ball.png")), (25, 25))
     ultraball = pygame.transform.scale(pygame.image.load(resource_path("assets/items/gen5/ultra-ball.png")), (25, 25))
     greatball = pygame.transform.scale(pygame.image.load(resource_path("assets/items/gen5/great-ball.png")), (25, 25))
